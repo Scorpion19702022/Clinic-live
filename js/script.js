d6e33b6c8d4c147d2af4a1f0116d3e402fb9f4ef
Jacek Mosiński <103215190+Scorpion19702022@users.noreply.github.com>
2023-05-06 23:22:38 +0200

    Add files via upload
    add all styles rwd for section about us

--- js/script.js
+++ js/script.js
@@ -10,6 +10,31 @@ const cross = document.querySelector('.fa-xmark')
 const body = document.querySelector('body')
 const infoHome = document.querySelector('.info')
 
+// ====================================================
+
+const boxCount = document.querySelector('.infomobile')
+const itemCount = document.querySelectorAll('.infocount__count')
+
+// console.log(boxCount)
+// console.log(itemCount)
+
+// ====================================================
+
+const actionAbout = document.querySelector('.box__info')
+
+const scrollInAboutUs = () => {
+	// console.log(window.scrollY)
+	setTimeout(() => {
+		if (window.scrollY >= 320) {
+			actionAbout.classList.add('actionaboutus')
+		} else {
+			actionAbout.classList.remove('actionaboutus')
+		}
+	}, 500)
+}
+
+// ====================================================
+
 body.onload = function () {
 	loadInfo()
 }
@@ -19,6 +44,8 @@ const loadInfo = () => {
 		infoHome.classList.add('actioninfo')
 	}, 800)
 }
+
+// ====================================================
 
 const handleBtn = () => {
 	nav.classList.toggle('actionnav')
@@ -33,4 +60,51 @@ const handleBtn = () => {
 	})
 }
 
+// ====================================================
+
+const optionCount = {
+	rootMargin: '-25%',
+}
+
+const noOptioCount = {
+	rootMargin: '-10%',
+}
+
+let final
+let value
+
+const startCounter = entry => {
+	// console.log(entry[0])
+	// console.log(entry[0].isIntersecting)
+
+	if (entry[0].isIntersecting) {
+		itemCount.forEach(count => {
+			// console.log(count)
+
+			const updateCount = () => {
+				final = count.getAttribute('data-number')
+				value = parseInt(count.textContent)
+
+				const speed = final / 10
+
+				if (value < final) {
+					count.textContent = `${Math.floor(value + speed)}`
+					setTimeout(updateCount, 50)
+				} else {
+					count.textContent = final
+				}
+			}
+
+			updateCount()
+		})
+	}
+}
+
+// ====================================================
+
+// ====================================================
+
 burgerBtn.addEventListener('click', handleBtn)
+const observer = new IntersectionObserver(startCounter, optionCount)
+observer.observe(boxCount)
+window.addEventListener('scroll', scrollInAboutUs)
