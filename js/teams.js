dc31b4450ef016bdddebfa9a4ff19588826bcc09
Jacek Mosiński <103215190+Scorpion19702022@users.noreply.github.com>
2023-05-24 22:56:06 +0200

    Add files via upload
    first attempt for slider in section teams

--- js/teams.js
+++ js/teams.js
@@ -18,4 +18,22 @@ const handleBtn = () => {
 	})
 }
 
+const swiper = new Swiper('.teamswiper', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
+
 burgerBtn.addEventListener('click', handleBtn)
